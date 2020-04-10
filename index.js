51f6a57e3ba2133b9029bdeec15ff7c30dc0863f
leifb <leif.bernsdorf@gmail.com>
2020-04-10 19:00:13 +0200

    export new publsiher and subscriber

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 module.exports.Worker = require('./lib/worker');
 module.exports.Client = require('./lib/client');
+module.exports.Subscriber = require('./lib/subscriber');
+module.exports.Publisher = require('./lib/publisher');
 module.exports.Defaults = require('./lib/defaults');
