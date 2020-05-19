7380e408d432b77ec837b37dbc1b1535589cc72d
leifb <leif.bernsdorf@gmail.com>
2020-05-19 14:43:55 +0200

    maybe this works

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -6,9 +6,11 @@ import Publisher from './lib/publisher';
 import * as Defaults from './lib/defaults';
 
 declare module "redis-request-broker" {
-	export const Worker;
-	export const Client;
-	export const Subscriber;
-	export const Publisher;
-	export const Defaults;
+	export {
+		Worker,
+		Client,
+		Subscriber,
+		Publisher,
+		Defaults
+	}
 }
